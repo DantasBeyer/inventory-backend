a6a22c5203cc071e9729ddcd1dd67a7c712c424e
Dantas Beyer <dantasbeyer@hotmail.com>
2023-03-29 18:17:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv").config () ;
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require ("cors");
+ 
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+//Connect to DB and start server
+mongoose
+    .connect(process.env.MONGO_URI)
+    .them (() => {
+        console.log(`Server Running on port ${PORT}`);
+    })
+    .catch((err) => console.log(err));
